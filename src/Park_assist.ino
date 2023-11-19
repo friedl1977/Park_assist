035340a2367d8263b0a7ecde45c1de2efe1fe232
Friedl Basson <friedlb@icloud.com>
2023-11-19 10:37:09 +0200

    New Project - Initial Commit

---
+++ src/Park_assist.ino
@@ -0,0 +1,18 @@
+/*
+ * Project Park_assist
+ * Description:
+ * Author:
+ * Date:
+ */
+
+
+//-- Instanciate Single Sensor
+
+//-- Setup
+void setup(){
+  Serial.begin(115200); }
+
+//-- Runtime.
+void loop() {
+
+}
